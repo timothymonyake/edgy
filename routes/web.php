1e63bdf6581a5ccc87143c3110f0874932d8f9c7
Timothy <timothymonyake@gmail.com>
2025-03-04 06:31:37 +0200

    wrapped routes inside of auth

--- routes/web.php
+++ routes/web.php
@@ -24,41 +24,44 @@ Route::get('/dashboard', function () {
 
 
 
-Route::get('pairs/all', [PairController::class, 'getPairs'])->name('pairs.all');
-Route::resource('pairs', PairController::class);
-
-Route::get('/trades/all', [TradeController::class, 'getTrades'])->name('trades.all');
-Route::resource('/trades', TradeController::class);
-
-Route::get('/phases/all', [PhaseController::class, 'getPhases'])->name('phases.all');
-Route::resource('/phases', PhaseController::class);
-
-Route::get('/kill-zones/all', [KillZoneController::class, 'getKillzones'])->name('killzones.all');
-Route::resource('kill-zones', KillZoneController::class);
-
-Route::get('/weekly-forecasts/seed-year-weeks', [WeeklyForecastController::class, 'seedYearWeeks'])->name('weeks.seed');
-Route::get('/weekly-forecasts/all', [WeeklyForecastController::class, 'getForecasts'])->name('weekly_forecasts.all');
-Route::resource('/weekly-forecasts', WeeklyForecastController::class);
-
-Route::get('/trading-plans/all', [TradingPlanController::class, 'getTradingPlans'])->name('trading_plans.all');
-Route::resource('/trading-plans',TradingPlanController::class);
-
-Route::get('/prop-firms/all', [PropFirmController::class, 'getPropFirms'])->name('prop_firms.all');
-Route::resource('/prop-firms', PropFirmController::class);
-
-Route::get('/lessons/all', [LessonController::class, 'getLessons'])->name('lessons.all');
-Route::resource('/lessons', LessonController::class);
-
-Route::get('/accounts/all', [AccountController::class, 'getAccounts'])->name('accounts.all');
-Route::resource('/accounts', AccountController::class);
-
-Route::get('/entry-checklists/all', [EntryChecklistController::class, 'getEntryChecklists'])->name('entry_checklists.all');
-Route::resource('/entry-checklists', EntryChecklistController::class);
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('pairs/all', [PairController::class, 'getPairs'])->name('pairs.all');
+    Route::resource('pairs', PairController::class);
+
+    Route::get('/trades/all', [TradeController::class, 'getTrades'])->name('trades.all');
+    Route::resource('/trades', TradeController::class);
+
+    Route::get('/phases/all', [PhaseController::class, 'getPhases'])->name('phases.all');
+    Route::resource('/phases', PhaseController::class);
+
+    Route::get('/kill-zones/all', [KillZoneController::class, 'getKillzones'])->name('killzones.all');
+    Route::resource('kill-zones', KillZoneController::class);
+
+    Route::get('/weekly-forecasts/seed-year-weeks', [WeeklyForecastController::class, 'seedYearWeeks'])->name('weeks.seed');
+    Route::get('/weekly-forecasts/all', [WeeklyForecastController::class, 'getForecasts'])->name('weekly_forecasts.all');
+    Route::resource('/weekly-forecasts', WeeklyForecastController::class);
+
+    Route::get('/trading-plans/all', [TradingPlanController::class, 'getTradingPlans'])->name('trading_plans.all');
+    Route::resource('/trading-plans', TradingPlanController::class);
+
+    Route::get('/prop-firms/all', [PropFirmController::class, 'getPropFirms'])->name('prop_firms.all');
+    Route::resource('/prop-firms', PropFirmController::class);
+
+    Route::get('/lessons/all', [LessonController::class, 'getLessons'])->name('lessons.all');
+    Route::resource('/lessons', LessonController::class);
+
+    Route::get('/accounts/all', [AccountController::class, 'getAccounts'])->name('accounts.all');
+    Route::resource('/accounts', AccountController::class);
+
+    Route::get('/entry-checklists/all', [EntryChecklistController::class, 'getEntryChecklists'])->name('entry_checklists.all');
+    Route::resource('/entry-checklists', EntryChecklistController::class);
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
